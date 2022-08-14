81039204f7cac253d4461f8fce5cd5756f62d389
HoodFast <rabota-trassa@mail.ru>
2022-08-15 00:36:16 +0300

    commit

--- foodgram/api_foodgram/permissions.py
+++ foodgram/api_foodgram/permissions.py
@@ -2,19 +2,15 @@ from rest_framework import permissions
 
 
 class AuthorOrReadOnly(permissions.BasePermission):
-
     def has_permission(self, request, view):
         return (
-            request.user.is_authenticated
-            or request.method in permissions.SAFE_METHODS
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated:
-            return (
-                request.user.is_admin()
-                or request.user.is_staff()
-                or request.user == obj.author
-                or request.method in permissions.SAFE_METHODS
-            )
-        return request.method in permissions.SAFE_METHODS
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_admin()
+        )

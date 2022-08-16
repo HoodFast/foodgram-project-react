6de9fdaa4cb4c9626f90f3752e88e09d20995320
HoodFast <rabota-trassa@mail.ru>
2022-08-16 10:26:02 +0300

    commit

--- foodgram/api_foodgram/urls.py
+++ foodgram/api_foodgram/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+
 from users.views import ChangePasswordView, SubscriptionViewSet, UserViewSet
 
 from .views import IngredientsViewSet, RecipesViewSet, TagsViewSet
--- foodgram/api_foodgram/views.py
+++ foodgram/api_foodgram/views.py
@@ -5,13 +5,14 @@ from django.db.models.expressions import Exists, OuterRef, Value
 from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 from reportlab.pdfgen import canvas
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import (SAFE_METHODS, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
+
+from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 
 from .filters import IngredientSearchFilter, RecipeFilter
 from .permissions import AuthorOrReadOnly
